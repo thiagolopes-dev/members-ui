b2946e610b507851e58197a88db7b34fd62d17c6
Thiago Lopes <thiagoolopezz@gmail.com>
2024-02-22 14:07:16 -0300

    add auth authentication

--- src/app/api.service.ts
+++ src/app/api.service.ts
@@ -7,7 +7,10 @@ import { Observable } from 'rxjs';
 })
 export class ApiService {
   apiUrl = 'http://localhost:8000';
-  httpHeaders = new HttpHeaders({ 'Content-type': 'application/json' });
+
+  token: string = 'Token 6e670b871af650062477a0dc196ce237eb1d547d';
+  httpHeaders = new HttpHeaders().set('Content-type', 'application/json')
+  .set('Authorization', this.token)
 
   constructor(private http: HttpClient) { }
 
--- src/app/member-detail/member.service.ts
+++ src/app/member-detail/member.service.ts
@@ -5,7 +5,9 @@ import { Observable } from 'rxjs';
 @Injectable()
 export class MemberService {
     apiUrl = 'http://localhost:8000';
-    httpHeaders = new HttpHeaders({ 'Content-type': 'application/json' });
+    token: string = 'Token 6e670b871af650062477a0dc196ce237eb1d547d';
+    httpHeaders = new HttpHeaders().set('Content-type', 'application/json')
+    .set('Authorization', this.token)
   
     constructor(private http: HttpClient) { }
   
